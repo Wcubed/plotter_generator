5d98e94f02be3f5b874df91644024163720d5488
Wybe Westra <wybe@ruurdwestra.nl>
2024-11-10 13:16:01 +0100

    Can output basic svg patterns.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{fs, str::FromStr};
 
 use camino::Utf8PathBuf;
-use color_eyre::Result;
+use chrono::{DateTime, Local};
+use color_eyre::{eyre::Context, Result};
 use log::LevelFilter;
 use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
 use svg::{
@@ -23,20 +24,42 @@ fn main() -> Result<()> {
     )?;
 
     let output_dir = Utf8PathBuf::from_str(OUTPUT_DIR)?;
-    fs::create_dir(&output_dir)?;
+    if !output_dir.exists() {
+        fs::create_dir(&output_dir)?;
+    }
 
-    let data = Data::new().move_to((10, 10)).line_to((10, 50));
+    let height = 70;
+    let width = 70;
+    let path_amount = 10;
 
-    let path = Path::new()
-        .set("fill", "none")
-        .set("stroke", "black")
-        .set("stroke-width", "3")
-        .set("d", data);
+    let mut document = Document::new().set("viewBox", (0, 0, width, height));
 
-    let document = Document::new().set("viewBox", (0, 0, 70, 70)).add(path);
+    let x_spacing = width / path_amount;
 
-    let output_file = output_dir.join("test.svg");
-    svg::save(output_file, &document)?;
+    for index in 0..path_amount {
+        document = document.add(generate_path(index * x_spacing, height));
+    }
+
+    let local_time = Local::now();
+    let timestamp = local_time.format("%Y-%m-%d_%H-%M-%S");
+
+    let output_file = output_dir.join(format!("test_{}.svg", timestamp));
+    svg::save(&output_file, &document)
+        .wrap_err_with(|| format!("Could not save as `{output_file}`"))?;
 
     Ok(())
+}
+
+fn generate_path(x_start: i32, y_end: i32) -> Path {
+    let data = Data::new()
+        .move_to((x_start, 0))
+        .line_to((x_start, y_end / 2 - 10))
+        .line_to((x_start + 10, y_end / 2 + 10))
+        .line_to((x_start + 10, y_end));
+
+    Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", "1")
+        .set("d", data)
 }
